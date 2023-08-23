a8316e61c912835cd0e52fcd30367913986e7815
wjollie <william.jollie23@gmail.com>
2023-08-23 19:47:35 -0400

    -bug fixed (hopefully)

--- seeds/index.js
+++ seeds/index.js
@@ -1,24 +1,43 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+const seedCategories = require("./category-seeds");
+const seedProducts = require("./product-seeds");
+const seedTags = require("./tag-seeds");
+const seedProductTags = require("./product-tag-seeds");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const { Category, Product, Tag, ProductTag } = require("../models");
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  try {
+    await sequelize.query("SET FOREIGN_KEY_CHECKS = 0", { raw: true });
+    await sequelize.sync({ force: true });
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+    // Drop the tables in the reverse order they were created
+    await ProductTag.drop();
+    await Tag.drop();
+    await Product.drop();
+    await Category.drop();
 
-  await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+    // Enable foreign key checks
+    await sequelize.query("SET FOREIGN_KEY_CHECKS = 1", { raw: true });
 
-  await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+    // Sync models after dropping tables
+    await sequelize.sync({ force: true });
+    console.log("\n----- DATABASE SYNCED -----\n");
+
+    await seedCategories();
+    console.log("\n----- CATEGORIES SEEDED -----\n");
+
+    await seedProducts();
+    console.log("\n----- PRODUCTS SEEDED -----\n");
+
+    await seedTags();
+    console.log("\n----- TAGS SEEDED -----\n");
+
+    await seedProductTags();
+    console.log("\n----- PRODUCT TAGS SEEDED -----\n");
+  } catch (err) {
+    console.error("Error seeding database:", err);
+  }
 
   process.exit(0);
 };
