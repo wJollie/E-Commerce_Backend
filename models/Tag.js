78709f9a613708e9db94f584f80a21b25d1df81f
wjollie <william.jollie23@gmail.com>
2023-08-23 19:13:56 -0400

    -added models folder with Product.js, ProductTag.js, Tag.js, and index.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,27 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
